28eb3252fa775926143c67833f106e2a94ddfa54
preya <ppatel68@una.edu>
2025-07-11 08:40:03 -0500

    Update and rename script.js to scripts/scripts.js

--- script.js
+++
@@ -1,2 +0,0 @@
-console.log("Welcome to Preya Patel's Dev Profile!");
-
---
+++ scripts/scripts.js
@@ -0,0 +1,31 @@
+body {
+  font-family: 'Roboto', sans-serif;
+  background: url('https://www.toptal.com/designers/subtlepatterns/uploads/double-bubble-outline.png');
+  margin: 0;
+  padding: 0;
+}
+
+.img-fluid {
+  max-width: 100%;
+  height: auto;
+}
+
+.nav-link {
+  cursor: pointer;
+}
+
+section {
+  display: none;
+}
+
+section.active {
+  display: block;
+}
+
+footer {
+  margin-top: 40px;
+  padding: 20px 0;
+  background-color: #212529;
+  color: white;
+  text-align: center;
+}
